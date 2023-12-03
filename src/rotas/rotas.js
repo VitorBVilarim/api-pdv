8893aee0fce6b793f65c09beca1861631bfd59f8
erickalvdev <alvaradoerick26@gmail.com>
2023-12-03 16:53:53 -0300

    Criação dos endpoints de cadastrar usuario e login, junto com os intermediarios

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -2,8 +2,17 @@ const express = require('express')
 const rotas = express()
 
 const { listarCategorias } = require('../controladores/categorias')
+const { cadastrarUsuario, login } = require('../controladores/usuarios')
+const validarCorpoRequisicao = require('../intermediarios/validar-corpo-requisicao')
+const schemaUsuario = require('../intermediarios/schema-usuario')
+const verificarLogin = require('../intermediarios/verificar-login')
 
+rotas.post('/usuario', validarCorpoRequisicao(schemaUsuario), cadastrarUsuario)
+rotas.post('/login', login)
+
+rotas.use(verificarLogin)
 
 rotas.get('/categorias', listarCategorias)
+
 
 module.exports = rotas
