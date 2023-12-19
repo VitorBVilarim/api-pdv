e975208d0f82ade39591e2398ccb032ad35f8da8
erickalvdev <alvaradoerick26@gmail.com>
2023-12-19 12:06:51 -0300

    corrigindo erro na resposta de listar pedidos

--- src/controladores/pedido.js
+++ src/controladores/pedido.js
@@ -2,93 +2,94 @@ const knex = require('../conexao/conexao');
 const { enviarEmail } = require('../servicos/nodemailer');
 
 async function cadastrarPedido(req, res) {
-    const { cliente_id, observacao, pedido_produtos } = req.body
+    const { cliente_id, observacao, pedido_produtos } = req.body;
 
     try {
         const clienteExistente = await knex('clientes').where('id', cliente_id).first();
 
         if (!clienteExistente) {
-            return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
+            return res.status(404).json({ mensagem: 'Cliente n達o encontrado.' });
         }
 
-        const cliente = await knex('clientes').where({ id: cliente_id })
+        const cliente = await knex('clientes').where({ id: cliente_id });
 
         if (cliente.length < 1) {
-            return res.status(404).json({ message: 'o Cliente informado não consta no nosso sistema!' })
+            return res.status(404).json({ message: 'O cliente informado n達o consta no nosso sistema!' });
         }
 
-        let valor_total = 0
-        let indexErro = 0
-        let indexErroProdutoInexistente = 0
-        let indexErroProdutoSemEstoque = 0
-
+        let valor_total = 0;
+        let indexErro = 0;
+        let indexErroProdutoInexistente = 0;
+        let indexErroProdutoSemEstoque = 0;
 
         const validacaoProduto = await Promise.all(pedido_produtos.map(async produto => {
-            let produtoExistente = await knex('produtos').where({ id: produto.produto_id }).returning('*')
-
+            let produtoExistente = await knex('produtos').where({ id: produto.produto_id }).returning('*');
 
             if (produtoExistente.length < 1) {
-                indexErroProdutoInexistente += 1
-                indexErro += 1
+                indexErroProdutoInexistente += 1;
+                indexErro += 1;
 
-                return 'produtoInexistente'
+                return 'produtoInexistente';
             }
             if (produtoExistente[0].quantidade_estoque < produto.quantidade_produto) {
-                indexErroProdutoSemEstoque += 1
-                indexErro += 1
+                indexErroProdutoSemEstoque += 1;
+                indexErro += 1;
 
-                return false
+                return false;
             } else if (indexErro < 1) {
+                valor_total += produtoExistente[0].valor * produto.quantidade_produto;
 
-                valor_total += produtoExistente[0].valor * produto.quantidade_produto
-
-                const produto_id = produto.produto_id
-                const quantidade_produto = produto.quantidade_produto
-                const valor = produtoExistente[0].valor
+                const produto_id = produto.produto_id;
+                const quantidade_produto = produto.quantidade_produto;
+                const valor = produtoExistente[0].valor;
 
                 return {
                     produto_id,
                     quantidade_produto,
-                    valor
-                }
+                    valor,
+                };
             }
         }));
 
-        const verificEstoque = validacaoProduto.includes(false)
-        const verificExistencia = validacaoProduto.includes('produtoInexistente')
+        const verificEstoque = validacaoProduto.includes(false);
+        const verificExistencia = validacaoProduto.includes('produtoInexistente');
 
         if (verificEstoque) {
-            return res.status(400).json({ mensagem: `Estoque insuficiente para o produto.` });
+            return res.status(400).json({ mensagem: 'Estoque insuficiente para o produto.' });
         }
 
         if (verificExistencia) {
-            return res.status(400).json({ mensagem: `o Produto informado não existe!` });
+            return res.status(400).json({ mensagem: 'O produto informado n達o existe!' });
         }
 
         const pedidoCadastrado = await knex('pedidos')
             .insert({
                 cliente_id,
                 observacao,
-                valor_total
+                valor_total,
             })
             .returning('id');
 
-        await knex('pedido_produtos').insert(validacaoProduto.map(produto => ({
-            pedido_id: pedidoCadastrado[0].id,
-            produto_id: produto.produto_id,
-            quantidade_produto: produto.quantidade_produto,
-            valor_produto: produto.valor
-        })));
+        await knex('pedido_produtos').insert(
+            validacaoProduto.map(produto => ({
+                pedido_id: pedidoCadastrado[0].id,
+                produto_id: produto.produto_id,
+                quantidade_produto: produto.quantidade_produto,
+                valor_produto: produto.valor,
+            }))
+        );
 
         validacaoProduto.map(async produto => {
-            await knex('produtos').where('id', produto.produto_id).decrement('quantidade_estoque', produto.quantidade_produto);
-        })
+            await knex('produtos')
+                .where('id', produto.produto_id)
+                .decrement('quantidade_estoque', produto.quantidade_produto);
+        });
 
         await enviarEmail(clienteExistente.email, 'Pedido Efetuado com Sucesso', 'Obrigado por fazer o pedido!');
 
         return res.status(201).json({ mensagem: 'Pedido cadastrado com sucesso.' });
     } catch (error) {
-        return res.status(500).json({ message: 'Erro interno no servidor!' })
+        return res.status(500).json({ message: 'Erro interno no servidor!' });
     }
 }
 
@@ -97,15 +98,24 @@ const listarPedidos = async (req, res) => {
 
     try {
         let query = knex('pedidos')
-            .select('pedidos.id', 'pedidos.valor_total', 'pedidos.observacao', 'pedidos.cliente_id',
-                'pedido_produtos.quantidade_produto', 'pedido_produtos.valor_produto', 'pedido_produtos.pedido_id', 'pedido_produtos.produto_id')
-            .join('pedido_produtos', 'pedidos.id', '=', 'pedido_produtos.pedido_id')
+            .select(
+                'pedidos.id',
+                'pedidos.valor_total',
+                'pedidos.observacao',
+                'pedidos.cliente_id',
+                'pedido_produtos.id as pp_id',
+                'pedido_produtos.quantidade_produto',
+                'pedido_produtos.valor_produto',
+                'pedido_produtos.pedido_id',
+                'pedido_produtos.produto_id'
+            )
+            .join('pedido_produtos', 'pedidos.id', '=', 'pedido_produtos.pedido_id');
 
         if (cliente_id) {
             const clienteExistente = await knex('clientes').where('id', cliente_id).first();
 
             if (!clienteExistente) {
-                return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
+                return res.status(404).json({ mensagem: 'Cliente n達o encontrado.' });
             }
 
             query = query.where('pedidos.cliente_id', cliente_id);
@@ -117,29 +127,44 @@ const listarPedidos = async (req, res) => {
             return res.status(404).json({ mensagem: 'Nenhum pedido encontrado.' });
         }
 
+        const resultadosFormatados = [];
+        const pedidosMapeados = new Map();
 
-        const resultadosFormatados = pedidos.map(pedido => {
-            return {
-                pedido: {
-                    id: pedido.id,
-                    valor_total: pedido.valor_total,
-                    observacao: pedido.observacao,
-                    cliente_id: pedido.cliente_id,
-                },
-                pedido_produtos: pedidos
-                    .filter(p => p.id === pedido.id)
-                    .map(p => ({
-                        id: p.id,
-                        quantidade_produto: p.quantidade_produto,
-                        valor_produto: p.valor_produto,
-                        pedido_id: p.pedido_id,
-                        produto_id: p.produto_id,
-                    })),
-            };
+        pedidos.forEach(pedido => {
+            const pedidoExistente = pedidosMapeados.get(pedido.id);
+
+            if (!pedidoExistente) {
+                const novoPedidoFormatado = {
+                    pedido: {
+                        id: pedido.id,
+                        valor_total: pedido.valor_total,
+                        observacao: pedido.observacao,
+                        cliente_id: pedido.cliente_id,
+                    },
+                    pedido_produtos: [
+                        {
+                            id: pedido.pp_id,
+                            quantidade_produto: pedido.quantidade_produto,
+                            valor_produto: pedido.valor_produto,
+                            pedido_id: pedido.pedido_id,
+                            produto_id: pedido.produto_id,
+                        },
+                    ],
+                };
+
+                resultadosFormatados.push(novoPedidoFormatado);
+                pedidosMapeados.set(pedido.id, novoPedidoFormatado);
+            } else {
+                pedidoExistente.pedido_produtos.push({
+                    id: pedido.pp_id,
+                    quantidade_produto: pedido.quantidade_produto,
+                    valor_produto: pedido.valor_produto,
+                    pedido_id: pedido.pedido_id,
+                    produto_id: pedido.produto_id,
+                });
+            }
         });
-
         return res.status(200).json(resultadosFormatados);
-
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
     }
